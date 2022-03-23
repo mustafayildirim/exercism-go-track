3be57fba6ae4ba993adffc755306dc0beb7cebb1
Mustafa YILDIRIM <mustafa.mym9@gmail.com>
2022-03-23 15:58:40 +0300

    solve bank-account

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -2,9 +2,8 @@ package account
 
 import "sync"
 
-// Define the Account type here.
 type Account struct {
-	mu     sync.Mutex
+	sync.Mutex
 	Amount int64
 	Open   bool
 }
@@ -17,42 +16,39 @@ func Open(amount int64) *Account {
 }
 
 func (a *Account) Balance() (int64, bool) {
-	a.mu.Lock()
+	a.Lock()
+	defer a.Unlock()
 	if a.Open {
-		a.mu.Unlock()
 		return a.Amount, a.Open
 	} else {
-		a.mu.Unlock()
 		return 0, false
 	}
 }
 
 func (a *Account) Deposit(amount int64) (int64, bool) {
 	if a.Open {
-		a.mu.Lock()
+		a.Lock()
+		defer a.Unlock()
 		a.Amount += amount
 		if a.Amount < 0 {
 			a.Amount = 0
-			a.mu.Unlock()
 			return 0, false
 		}
-		a.mu.Unlock()
 		return a.Amount, a.Open
 	} else {
 		return 0, false
 	}
 }
 func (a *Account) Close() (int64, bool) {
-	a.mu.Lock()
+	a.Lock()
+	defer a.Unlock()
 	// If account is Open -> close it
 	// else return false for the operation and leave it closed.
 	if a.Open {
 		a.Open = false
-		a.mu.Unlock()
 		return a.Amount, !a.Open
 	} else {
 		a.Amount = 0
-		a.mu.Unlock()
 		return a.Amount, a.Open
 	}
 }
